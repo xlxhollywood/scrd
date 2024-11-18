ea1ade500db9ebd037a565d0cd06d0d063baf8c2
ksehun <hsaint0325@naver.com>
2024-11-18 21:44:16 +0900

    init

---
+++ src/test/java/org/example/scrd/ScrdApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.scrd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
